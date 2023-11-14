d0c388064334d7b5d17b1a7bf77d4c89b5dd99f8
Enoh Collins <eno.col@gmail.com>
2023-11-14 13:27:03 +0000

    Edit teachers

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
 
   attr_accessor :specialization
-  def initialize(id, age, name: 'Unknown', specialization, parent_permission: parent_permission)
-    super(id, age, name: name, parent_permission)
+  def initialize(id, age, specialization, name: 'Unknown', parent_permission: false)
+    super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
