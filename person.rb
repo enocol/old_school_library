f802cb7aae126185b222f1731075512ae9828492
Enoh Collins <eno.col@gmail.com>
2023-11-14 14:02:17 +0000

    Edit person.rb

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-  attr_accessor :id, :name, :of_age
-  
+  attr_accessor :id, :name, :age
+
   def initialize(id, age, name: 'Unknown', parent_permission: true)
     @id = id
     @name = name
