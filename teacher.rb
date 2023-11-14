7fddaa76f379e7aedab6943da8048b1bdc3d73aa
Enoh Collins <eno.col@gmail.com>
2023-11-14 13:11:46 +0000

    Edit person.rb

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, age, name: 'Unknown', parent_permission: true, specialization:)
-    super(id, age, name: name, parent_permission: parent_permission)
+  def initialize(id, age, name: 'Unknown', specialization)
+    super(id, age, name: name)
     @specialization = specialization
   end
 
