ef70158aa4b2a197f754a8bc428175b92342d8a9
Enoh Collins <eno.col@gmail.com>
2023-11-15 11:03:56 +0000

    Fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -3,10 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age,  name: 'Unknown', parent_permission: false , specialization: nil)
+  def initialize(age, name: 'Unknown', parent_permission: false, specialization: nil)
     super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
-
 end
-
