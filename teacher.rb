59ab3620c8ebd7ff63b6e377ce49fd4069c8036b
Enoh Collins <eno.col@gmail.com>
2023-11-15 07:55:55 +0000

    Fix Bug

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,11 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age, specialization, name: 'Unknown', parent_permission: false)
-    super(id, age, name: name, parent_permission: parent_permission)
+  def initialize(age,  name: 'Unknown', parent_permission: false , specialization: nil)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
-  def can_use_services?
-    true
-  end
+
 end
+
