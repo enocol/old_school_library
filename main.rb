2fc252bc2caa9f38ab91c20a899739c513950253
Enoh Collins <eno.col@gmail.com>
2023-11-17 18:44:25 +0000

    Add and edit main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'app'
 
-person = Person.new(22, name: 'maximilianus')
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+app = App.new
+app.run
