8e2fc65904a813caebc757d478cdf6aa488a2e25
Enoh Collins <eno.col@gmail.com>
2023-11-17 18:45:04 +0000

    Refactor rental.rb and studen.rbt

--- student.rb
+++ student.rb
@@ -6,16 +6,10 @@ class Student < Person
   def initialize(age, name, parent_permission, classroom)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
-    #add_to_classroom_students if classroom
+    
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
-
-  private
-
- # def add_to_classroom_students
-  #  @classroom.students = self
-  #end
 end
