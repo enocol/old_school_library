4e237a15f843f784043f980364ca846427bb4251
Enoh Collins <eno.col@gmail.com>
2023-11-18 00:05:05 +0000

    Fix linters

--- teacher.rb
+++ teacher.rb
@@ -4,7 +4,7 @@ class Teacher < Person
   attr_accessor :specialization, :parent_permission
 
   def initialize(age, name, parent_permission, specialization)
-    super(age, name: name, parent_permission: parent_permission)
+    super(age, name:, parent_permission:)
     @specialization = specialization
   end
 end
